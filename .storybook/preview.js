9dc9454025be85283d63c1826499298f08779c56
macdaegeon <chleorjs37@gmail.com>
2024-02-04 19:26:37 +0900

    fix : 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,12 @@
 /** @type { import('@storybook/vue3').Preview } */
+
+import { setup } from '@storybook/vue3';
+import { createPinia } from 'pinia';
+import '../src/index.css';
+
+setup((app) => {
+  app.use(createPinia());
+});
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
---
+++ src/components/TaskList2.vue
@@ -0,0 +1,28 @@
+<template>
+    <PureTaskList :tasks="tasks" @archive-task="archiveTask" @pin-task="pinTask" />
+</template>
+<script>
+import PureTaskList from './PureTaskList.vue';
+import { computed } from 'vue';
+
+import { useTaskStore } from '../store';
+
+export default {
+    components: { PureTaskList },
+    name: "TaskList",
+    setup() {
+        const store = useTaskStore();
+
+        const tasks = computed(() => store.getFilteredTasks);
+
+        const archiveTask = (task) => store.archiveTask(task);
+        const pinTask = (task) => store.pinTask(task);
+
+        return {
+            tasks,
+            archiveTask,
+            pinTask
+        }
+    }
+}
+</script>
